c53b275106f9122f90a471fa06d7c61ec52c20d3
Marco Castro <marcodotcastro@gmail.com>
2024-10-17 16:13:32 -0300

    build: sidekiq

--- app/services/task_service.rb
+++ app/services/task_service.rb
@@ -3,7 +3,6 @@ class TaskService
   NOTIFICATION_URL = "#{ENV["URL_MS_TASK"]}/api/scrapings"
 
   def self.call(task_id, status, scraped_data = nil)
-    # TODO: Mover para HabbitMQ
     body = { task_id: task_id, status: status, scraped_data: scraped_data }
 
     HTTParty.put("#{NOTIFICATION_URL}/#{task_id}", body: body.to_json, headers: { 'Content-Type' => 'application/json' })
